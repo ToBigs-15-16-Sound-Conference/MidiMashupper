0aa18a0c7ba892d05dcbc97f7a9f9ad708f80b35
YMGYM <good_day1234@naver.com>
2021-12-22 03:01:30 +0900

    [FEATURE] pypianoroll 대응 전환

--- components/utils.py
+++ components/utils.py
@@ -1,124 +1,95 @@
 import numpy as np
-import music21
-from music21 import *
-from sklearn.metrics.pairwise import cosine_similarity
+import pypianoroll
 
 """
-미디를 불러오고 매시업 하기 위한 다양한 함수들이 내장되어 있습니다.
+pypianoroll 을 사용해서 파일을 불러오는 유틸리티 파일입니다.
 """
 
-def convert_numpy_midi(mf: music21.midi.MidiFile) -> np.ndarray:
+def load_as_np(path:str, beat_resolution:int=4, lowest_pitch:int=24, n_pitches:int=72)->np.ndarray:
     """
-    music21 midiFile을 입력받아서 이를 numpy 형태로 변환해서 반환합니다.
-    <입력>
-    mf: music21.midi.MidiFile. 모든 트랙 정보가 담겨 있는 미디 객체
+    pypianoroll 형식으로 npz 또는 midi 파일을 불러와서 마디별로 분할된 넘파이 형식으로 반환합니다.
 
-    <출력>
-    np.ndarray. shape: (len, pitch, velocity)
-    len은 틱 단위로 되어 있습니다.
-    pitch는 1~128 까지이며, 0인 경우 연주하지 않는 음입니다.
-    """
-    trackEvents = [] # 전체 playLists의 조합
-    for track in mf.tracks:
-        #노트의 시간, 음, 강도를 만들기
-        playLists = [] # 트랙별 모든 시간
-        dTime = 0 # 현재 시간단위
-        for e in track.events:
-            if type(e) == music21.midi.MidiEvent:
-                playLists.append([dTime, e.pitch if e.pitch is not None else -1, e.velocity if e.velocity is not None else -1])
-            elif type(e) == music21.midi.DeltaTime:
-                dTime += e.time # 빈 시간 단위가 나오면.. 커서 이동
-        trackEvents.append(playLists)
+    입력
+    ----------
+    path:str 파일 주소를 나타내는 패스 .npz 또는 .mid로 끝나야 함
+    beat_resolution: 한 마디당 타임 틱을 나타냅니다. 논문 기본값은 4입니다.
+    lowest_pitch: 가장 낮은 음은 24번째 음입니다.
+    n_pitches: 총 몇개의 음을 사용할지 결정합니다.
 
-    musicLen = max(x[-1][0] for x in trackEvents) + 1 # 전체 트랙 중에 가장 긴 timelength 를 가지는 시간을 찾기
-    npLists = np.zeros((musicLen, 128, 1), dtype=np.int8) # 음악 길이, 피치 범위, velocity
-
-    for track in trackEvents: # 트랙별로 값을 입력합니다.
-        for notes in track:
-            npLists[notes[0], notes[1] + 1] = notes[2] + 1
-
-    return npLists
-
-
-def open_midi(file_path: str, stream: bool = False) -> music21.midi.MidiFile:
-    """
-    music21 midiFile을 입력받아서 이를 numpy 형태로 변환한 뒤, 한 마디 단위로 분할하여 유사도를 비교할 수 있게 합니다.
-    <입력>
-    file_path: str ".midi"로 끝나는 확장자
-    stream: boolean. stream 형태로 불러오는지 설정합니다. 
-
-    <출력>
-    music21 midiFile
+    출력
+    ----------
+    마디 별로 분할된 넘파이 배열
+    shape : (마디, 트랙, 마디당 틱, 피치)
     """
 
-    try:
-        mf = midi.MidiFile()
-        mf.open(file_path) # 파싱 준비
-        mf.read() # 파싱과 스트림
-        mf.close() # 파일 메모리 분리
+    if path[:-3] == 'npz':
+        score = pypianoroll.load(path)
+    else:
+        score = pypianoroll.read(path)
 
-        if stream:
-            return midi.translate.midiFileToStream(mf)
-        else:
-            return mf
-    except Exception as e:
-        print(f"Error on Loading Midi file! : {e}")
-        
-        return None
+    # score.binarize() # 이진화 실시(어떤 이점이 있는지 모르겠습니다.)
+    score.set_resolution(beat_resolution) # 비트당 틱을 재설정합니다.
+
+    # 넘파이 배열로 변경합니다.
+    pianoroll = (score.stack() > 0) # .stack() -> (track, time, pitch)
+
+    # 주어진 피치범위로 잘라냅니다.
+    pianoroll = pianoroll[:, :, lowest_pitch:lowest_pitch+n_pitches] # (track, time, 피치 수)
+
+    # 전체 마디 수를 계산합니다.
+    measure_resolution = 4 * beat_resolution # 한 마디는 4박이기 때문에 마디당 틱 수를 계산할 수 있습니다.
+    n_measures = score.get_max_length() // measure_resolution
+
+    # 마디별로 배열을 잘라서 저장합니다.
+    split = []
+    for i in range(0, n_measures, measure_resolution):
+        split.append(pianoroll[:, i:i+measure_resolution, :])
+
+    
+    return np.stack(split) # shape: (마디, 트랙, 틱, 피치)
 
 
-def split_per_bars(mf: music21.midi.MidiFile) -> np.ndarray:
+ def concat_midi(score1:pypianoroll.Multitrack, pos1: int, score2:pypianoroll.Multitrack, pos2:int, pad:int=2)->pypianoroll.Multitrack:
     """
-    music21 midiFile을 입력받아서 이를 numpy 형태로 변환해서 반환합니다.
-    <입력>
-    mf: music21.midi.MidiFile. 모든 트랙 정보가 담겨 있는 미디 객체
+    pypianoroll 형식 파일 두 개를 받아서 두 지점을 이어 새로운 미디 파일로 만듭니다.
+    score1은 먼저 나온 뒤 pos1 지점에서 끊기고, score2의 pos2지점에서 이어서 나옵니다.
 
-    <출력>
-    np.ndarray. shape: (bars, len, pitch, velocity)
-    bars는 전체 틱 수 / 한 마디당 틱 수의 올림 변환으로 구분됩니다.
-    len은 틱 단위로 되어 있습니다.
-    pitch는 1~128 까지이며, 0인 경우 연주하지 않는 음입니다.
-    """
+    입력
+    ----------
+    score1: 첫 번째 멀티트랙 파일입니다. 먼저 재생될 미디 파일입니다.
+    pos1: score1의 매시업 포인트입니다.
+    score2: 두 번째 멀티트랙 파일입니다. 나중에 재생됩니다.
+    pos2: score2의 매시업 포인트입니다.
+    pad: pos1, pos2 이전/이후 몇 마디씩을 남기고 자를 것인지를 결정합니다.
 
-    npSong = convert_numpy_midi(mf) # 넘파이 형태로 분할합니다.
-    barTicks = (mf.ticksPerQuarterNote * 4) # 4/4 박자로 가정합니다.
-
-    splited_song = []
-    for i in range(int(len(npSong)/barTicks)):
-        splited_song.append(npSong[i*barTicks:(i+1)*barTicks]) 
-
-    return np.array(splited_song)
-
-def midi_cosine_similarity(split1, split2):
-    """
-    마디 단위로 나눠진 파일을 입력 받아 유사도를 계산합니다.
-    <입력>
-    split1, split2 : np.ndarray. shape: (len, pitch, velocity), split_per_bars의 결과값
-
-    <출력>
-    np.ndarray. shape: (len(split1), len(split2))
-    각 마디 별 코사인 유사도를 구합니다.
-
+    출력
+    ----------
+    pypianoroll.Multitrack. 
     """
 
-    similarity = np.empty((len(split1),len(split2)), dtype=np.float32)
-    for i, barX in enumerate(split1):
-        for j, barY in enumerate(split2):
-            similarity[i][j] = cosine_similarity(barX.reshape(1,-1), barY.reshape(1,-1))[0][0] # 속도가 느림.. 개선 필요
+    # 원본 손상을 방지하기 위해 복사해서 사용합니다
+    score1_tmp = score1.copy()
+    score2_tmp = score2.copy()
 
-    return similarity
+    # 최대 길이를 찾아서 이에 맞게 패딩합니다.
+    score1_max = score1.get_max_length()
+    score2_max = score2.get_max_length()
+    score1_tmp.pad_to_same(score1_max)
+    score2_tmp.pad_to_same(score2_max)
 
-def convert
-def key_transform_12(mf):
-    """
-    마디 단위로 나눠진 파일을 입력 받아 12개 키로 변조된 파일을 반환합니다.
-    """
+    # 트림 가능한 최대 거리를 구합니다.
+    leftStart = min(0, pos1-pad)
+    rightFinish = max(score2_max, pos2+pad)
 
-    pass
+    # 길이만큼 자르기
+    score1_tmp.trim(leftStart, pos1)
+    score2_tmp.trim(pos2, rightFinish)
 
-def get_chords(df):
-    """
-    마디 단위로 나눠진 파일을 입력 받아 각 마디에서 추정된 코드를 반환합니다.
-    """
+    return score1_tmp, score2_tmp # 이 두개를 연결할 수 있는 방법이 없을지 살펴볼 필요성이 있다.
 
-    pass
+
+
+
+
+
+
---
+++ components/utils_old.py
@@ -0,0 +1,129 @@
+import numpy as np
+import music21
+from music21 import *
+from sklearn.metrics.pairwise import cosine_similarity
+
+"""
+미디를 불러오고 매시업 하기 위한 다양한 함수들이 내장되어 있습니다.
+pypianoroll을 사용하여 현재는 사용하지 않는 함수들입니다.
+"""
+
+def convert_numpy_midi(mf: music21.midi.MidiFile) -> np.ndarray:
+    """
+    music21 midiFile을 입력받아서 이를 numpy 형태로 변환해서 반환합니다.
+    <입력>
+    mf: music21.midi.MidiFile. 모든 트랙 정보가 담겨 있는 미디 객체
+
+    <출력>
+    np.ndarray. shape: (len, pitch, velocity)
+    len은 틱 단위로 되어 있습니다.
+    pitch는 1~128 까지이며, 0인 경우 연주하지 않는 음입니다.
+    """
+    trackEvents = [] # 전체 playLists의 조합
+    for track in mf.tracks:
+        #노트의 시간, 음, 강도를 만들기
+        playLists = [] # 트랙별 모든 시간
+        dTime = 0 # 현재 시간단위
+        for e in track.events:
+            if type(e) == music21.midi.MidiEvent:
+                playLists.append([dTime, e.pitch if e.pitch is not None else -1, e.velocity if e.velocity is not None else -1])
+            elif type(e) == music21.midi.DeltaTime:
+                dTime += e.time # 빈 시간 단위가 나오면.. 커서 이동
+        trackEvents.append(playLists)
+
+    musicLen = max(x[-1][0] for x in trackEvents) + 1 # 전체 트랙 중에 가장 긴 timelength 를 가지는 시간을 찾기
+    npLists = np.zeros((musicLen, 128, 1), dtype=np.int8) # 음악 길이, 피치 범위, velocity
+
+    for track in trackEvents: # 트랙별로 값을 입력합니다.
+        for notes in track:
+            npLists[notes[0], notes[1] + 1] = notes[2] + 1
+
+    return npLists
+
+
+def open_midi(file_path: str, stream: bool = False) -> music21.midi.MidiFile:
+    """
+    music21 midiFile을 입력받아서 이를 numpy 형태로 변환한 뒤, 한 마디 단위로 분할하여 유사도를 비교할 수 있게 합니다.
+    <입력>
+    file_path: str ".midi"로 끝나는 확장자
+    stream: boolean. stream 형태로 불러오는지 설정합니다. 
+
+    <출력>
+    music21 midiFile
+    """
+
+    try:
+        mf = midi.MidiFile()
+        mf.open(file_path) # 파싱 준비
+        mf.read() # 파싱과 스트림
+        mf.close() # 파일 메모리 분리
+
+        if stream:
+            return midi.translate.midiFileToStream(mf)
+        else:
+            return mf
+    except Exception as e:
+        print(f"Error on Loading Midi file! : {e}")
+        
+        return None
+
+
+def split_per_bars(mf: music21.midi.MidiFile) -> np.ndarray:
+    """
+    music21 midiFile을 입력받아서 이를 numpy 형태로 변환해서 반환합니다.
+    <입력>
+    mf: music21.midi.MidiFile. 모든 트랙 정보가 담겨 있는 미디 객체
+
+    <출력>
+    np.ndarray. shape: (bars, len, pitch, velocity)
+    bars는 전체 틱 수 / 한 마디당 틱 수의 올림 변환으로 구분됩니다.
+    len은 틱 단위로 되어 있습니다.
+    pitch는 1~128 까지이며, 0인 경우 연주하지 않는 음입니다.
+    """
+
+    npSong = convert_numpy_midi(mf) # 넘파이 형태로 분할합니다.
+    barTicks = (mf.ticksPerQuarterNote * 4) # 4/4 박자로 가정합니다.
+
+    splited_song = []
+    for i in range(int(len(npSong)/barTicks)):
+        splited_song.append(npSong[i*barTicks:(i+1)*barTicks]) 
+
+    return np.array(splited_song)
+
+def midi_cosine_similarity(split1, split2):
+    """
+    마디 단위로 나눠진 파일을 입력 받아 유사도를 계산합니다.
+    <입력>
+    split1, split2 : np.ndarray. shape: (len, pitch, velocity), split_per_bars의 결과값
+
+    <출력>
+    np.ndarray. shape: (len(split1), len(split2))
+    각 마디 별 코사인 유사도를 구합니다.
+
+    """
+
+    similarity = np.empty((len(split1),len(split2)), dtype=np.float32)
+    for i, barX in enumerate(split1):
+        for j, barY in enumerate(split2):
+            similarity[i][j] = cosine_similarity(barX.reshape(1,-1), barY.reshape(1,-1))[0][0] # 속도가 느림.. 개선 필요
+
+    return similarity
+
+def key_transform_12(mf):
+    """
+    마디 단위로 나눠진 파일을 입력 받아 12개 키로 변조된 파일을 반환합니다.
+    """
+
+    pass
+
+def get_pitches(s: music21.Stream):
+    """
+    마디 단위로 나눠진 파일을 입력 받아 각 마디에서 추정된 코드를 반환합니다.
+    <입력>
+    s: music21.Stream. 모든 트랙 정보가 담겨 있는 미디 객체 (스트림 파일)
+
+    <출력>
+    피치 확인된 음정들
+    """
+
+    return [str(p) for p in s.pitches]
